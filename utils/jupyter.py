66063c09b1c815ce54bb3f2881b63d2d17ded11b
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-08-29 03:52:46 +0200

    fixed imports

--- utils/jupyter.py
+++ utils/jupyter.py
@@ -1,5 +1,11 @@
 """ Provides some utilities to deal with jupyter notebook specific issues """
 
+from inspect import getsource
+from IPython.display import HTML, display,IFrame
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import PythonLexer
+from pygments import highlight
+
 def source(*functions):
     """ source a script as if we have written the script in jupyter notebook and executed it """
     source_code = '\n\n'.join(getsource(fn) for fn in functions)        

79d79edf6ccb9c766fae2e1b7c7e97955e7185b1
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-08-29 03:42:35 +0200

    ant path

--- utils/viz.py
+++ utils/viz.py
@@ -24,6 +24,7 @@ def draw_map(G, highlight = None , zoom = 16):
         )
         m.add_layer(lines)
 
+    # if we want to mark some specific nodes
     if highlight:
         for node_osmid in highlight:
             node = nodes_frame.loc[node_osmid]
@@ -32,3 +33,39 @@ def draw_map(G, highlight = None , zoom = 16):
             m.add_layer(marker)
 
     return m
+
+def draw_route(G, route, zoom = 16):
+    center_osmid = ox.stats.extended_stats(G,ecc=True)['center'][0]
+    G_gdfs = ox.graph_to_gdfs(G)
+    nodes_frame = G_gdfs[0]
+    ways_frame = G_gdfs[1]
+    center_node = nodes_frame.loc[center_osmid]
+    location = (center_node['y'], center_node['x'])
+    m = lf.Map(center = location, zoom = 16)
+
+    start_node = nodes_frame.loc[route[0]]
+    end_node = nodes_frame.loc[route[len(route)-1]]
+
+    start_xy = (start_node['y'], start_node['x'])
+    end_xy = (end_node['y'], end_node['x'])
+    marker = lf.Marker(location = start_xy, draggable = False)
+    m.add_layer(marker)
+    marker = lf.Marker(location = end_xy, draggable = False)
+    m.add_layer(marker)
+
+    for u, v in zip(route[0:], route[1:]):
+        x, y = (ways_frame.query(f'u == {u} and v == {v}').to_dict('list')['geometry'])[0].coords.xy
+        points = map(list, [*zip([*y],[*x])])
+        ant_path = lf.AntPath(
+            locations = [*points], 
+            dash_array=[1, 10],
+            delay=1000,
+            color='red',
+            pulse_color='black'
+        )
+        m.add_layer(ant_path)
+
+    return m
+    
+
+

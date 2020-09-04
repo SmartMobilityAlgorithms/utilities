98477619ad899c4d932b0cc6e8800e67facfd495
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-09-05 00:53:45 +0200

    priority queue

--- utils/problem.py
+++ utils/problem.py
@@ -30,3 +30,31 @@ def astar_heuristic(G, origin, destination):
         distanceOrigin[node] = destDist
 
     return distanceGoal, distanceOrigin
+
+"""
+A Queue in which the minimum (or maximum) element (as determined by f and order) is returned first.
+If order is 'min', the item with minimum f(x) isreturned first; if order is 'max', then it is the item with maximum f(x).
+Also supports dict-like lookup.
+"""
+
+class PriorityQueue:
+    def __init__(self, order='min', f=lambda x: x):
+        self.heap = []
+        if order == 'min':
+            self.f = f
+        elif order == 'max':  # now item with max f(x)
+            self.f = lambda x: -f(x)  # will be popped first
+        else:
+            raise ValueError("Order must be either 'min' or 'max'.")
+
+    def append(self, item):
+        """Insert item at its correct position."""
+        heapq.heappush(self.heap, (self.f(item), item))
+
+    def pop(self):
+        """Pop and return the item (with min or max f(x) value)
+        depending on the order."""
+        if self.heap:
+            return heapq.heappop(self.heap)[1]
+        else:
+            raise Exception('Trying to pop from empty PriorityQueue.')

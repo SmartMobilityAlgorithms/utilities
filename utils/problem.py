3a551b70c899fc0ef4cde2fc60ee88a9cc81c012
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-09-05 01:09:14 +0200

    handling iterables

--- utils/problem.py
+++ utils/problem.py
@@ -56,6 +56,11 @@ class PriorityQueue:
         """Insert item at its correct position."""
         heapq.heappush(self.heap, (self.f(item), item))
 
+    def extend(self, items):
+        """Insert each item in items at its correct position."""
+        for item in items:
+            self.append(item)
+
     def pop(self):
         """Pop and return the item (with min or max f(x) value)
         depending on the order."""
@@ -63,3 +68,27 @@ class PriorityQueue:
             return heapq.heappop(self.heap)[1]
         else:
             raise Exception('Trying to pop from empty PriorityQueue.')
+
+    def __len__(self):
+        """Return current capacity of PriorityQueue."""
+        return len(self.heap)
+
+    def __contains__(self, key):
+        """Return True if the key is in PriorityQueue."""
+        return any([item == key for _, item in self.heap])
+
+    def __getitem__(self, key):
+        """Returns the first value associated with key in PriorityQueue.
+        Raises KeyError if key is not present."""
+        for value, item in self.heap:
+            if item == key:
+                return value
+        raise KeyError(str(key) + " is not in the priority queue")
+
+    def __delitem__(self, key):
+        """Delete the first occurrence of key."""
+        try:
+            del self.heap[[item == key for _, item in self.heap].index(True)]
+        except ValueError:
+            raise KeyError(str(key) + " is not in the priority queue")
+        heapq.heapify(self.heap)

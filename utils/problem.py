9e952c0ecf07ab2e632183d3a236ef57cb6d093a
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-09-12 07:09:18 +0200

    simulated annealing

--- utils/problem.py
+++ utils/problem.py
@@ -39,42 +39,9 @@ def astar_heuristic(G, origin, destination):
     return distanceGoal, distanceOrigin
 
 
-
 """
-Generate random simple path between source and destination node
-over a osmnx graph.
-
-We can use networkx.all_simple_paths iterator function which would serve the
-same purpose,but if you went to the its implementation you will see that they use stack
-of all the edges of the graph which would really hurt our performance when you use big graph
-over a complete city or something.
-
-Our method is randomized graph search, and the randomization is about randomly selecting
-the node to expand in each step, and only keeping the frontier in our memory.
-
-It is an iterator function so we don't overload our memory if you wanted a lot of paths.
+One possible schedule function for simulated annealing
 """
 
-def randomized_search(G, source, destination, nums_of_paths = 1):
-    origin = Node(graph = G, osmid = source)
-    destination = Node(graph = G, osmid = destination)
-    nums = nums_of_paths
-    
-    while nums > 0:
-        route = [] # the route to be yielded
-        frontier = deque([origin])
-        explored = set()
-        found = False
-        while frontier and not found:
-            node = random.choice(frontier)   # here is the randomization part
-            frontier.remove(node)
-            explored.add(node)
-
-            for child in node.expand():
-                if child not in explored and child not in frontier:
-                    if child == destination:
-                        route = child.path()
-                        yield route
-                        found = True
-                    frontier.append(child)
-        nums -= 1
+def exp_schedule(k=20, lam=0.005, limit=100):
+    return lambda t: (k * np.exp(-lam * t) if t < limit else 0)

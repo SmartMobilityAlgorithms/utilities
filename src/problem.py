e3ebc945bf03a448b8204edf94f3f17b3fe1a0c0
Omar Khaled <omarkhaled@Omars-MacBook-Pro.local>
2020-09-29 10:57:46 +0200

    generic A star

--- src/problem.py
+++ src/problem.py
@@ -8,18 +8,36 @@ from collections import deque
 import numpy as np
 
 
-"""
-Used in A-star algorithm; it takes the source and destination node
+
+########################################################################
+########################################################################
+############################# A STAR ###################################
+########################################################################
+########################################################################
+
+
+"""Used in A-star algorithm; it takes the source and destination node
 and calculate the summation of straight line distance between each node
 to origin and each node to the destination.
 
 The value of the summation is not the actual distance but up to the some actual scale as 
 we use matplotlib coordinates (x,y)
 
-return dictionary
+Parameters
+----------
+G: NetworkX graph returned from osmnx methods
+origin: The id of the origin node in the graph 
+destination: The id of the destination node in the graph
+measuring_dist: The method used in measuring distance between nodes; staright line distance,
+                harversine distance
+
+Returns
+-------
+distanceGoal, distanceOrigin: dictionaries that associate the proper distance with node id
+
 """
 
-def astar_heuristic(G, origin, destination):
+def astar_heuristic(G, origin, destination, measuring_dist = straight_line):
     distanceGoal = dict()
     distanceOrigin = dict()
 
@@ -33,21 +51,50 @@ def astar_heuristic(G, origin, destination):
         pointX = G.nodes[node]['x']
         pointY = G.nodes[node]['y']
 
-        originDest = math.sqrt((pointX-originX)**2 + (pointY-originY)**2)
-        destDist = math.sqrt((pointX-destX)**2 + (pointY-destY)**2)
-        
+        originDist = measuring_dist(originX, originY, pointX, pointY)
+        destDist = measuring_dist(pointX, pointY, destX, destY)
+
         distanceGoal[node] = originDest
         distanceOrigin[node] = destDist
 
     return distanceGoal, distanceOrigin
 
 
+
+"""Calculates the straight line distance between two points on earth
+(specified in decimal degrees). This is of course an approximation, but 
+acceptable one if the two points are close to each other
+
+Parameters
+----------
+lon1: longitude of the first point
+lat1: latitude of the second point
+lon2: longitude of the second point
+lat2: latitude of the second point
+
+Returns:
+distance: the straight line distance calculated by pythagoras theorem
+
 """
-Calculates the great circle distance between two points  on the earth
+def straight_line(lon1, lat1, lon2, lat2):
+    return math.sqrt((lon2 - lon1)**2 + (lat2-lat1)**2)
+
+
+
+"""Calculates the great circle distance between two points  on the earth
 (specified in decimal degrees). This is useful when finding distance between 
 two points that are far-away. 
 
-return distance
+Parameters
+----------
+lon1: longitude of the first point
+lat1: latitude of the second point
+lon2: longitude of the second point
+lat2: latitude of the second point
+
+Returns:
+distance: the distance of the great circle arc between two points
+          calculated by harversine method
 """
 
 def haversine_distance (lon1, lat1, lon2, lat2):
@@ -64,39 +111,20 @@ def haversine_distance (lon1, lat1, lon2, lat2):
     return c * r
 
 
-"""
-Used in A-star algorithm; it takes the source and destination node
-and determines the great-circle distance between two points on a sphere
-This is calculated for each node and to origin and each node to the destination.
 
-return dictionary
-"""
-
-def astar_heuristic_haversine(G, origin, destination):
-    distanceGoal = dict()
-    distanceOrigin = dict()
-
-    originX = G.nodes[origin]['x']
-    originY = G.nodes[origin]['y']
-
-    destX = G.nodes[destination]['x']
-    destY = G.nodes[destination]['y']
-
-    for node in G:
-        pointX = G.nodes[node]['x']
-        pointY = G.nodes[node]['y']
-
-        originDest = haversine_distance(originX, originY, pointX, pointY)
-        destDist = haversine_distance(pointX, pointY, destX, destY)
-
-        distanceGoal[node] = originDest
-        distanceOrigin[node] = destDist
-
-    return distanceGoal, distanceOrigin
+########################################################################
+########################################################################
 
 
-"""
-One possible schedule function for simulated annealing
+########################################################################
+########################################################################
+############################# Simulated Annealing ######################
+########################################################################
+########################################################################
+
+
+
+"""Possible schedule function for simulated annealing
 """
 
 def exp_schedule(k=20, lam=0.005, limit=100):

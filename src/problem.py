2d7565da40331777411e802d344bcd7871f9c3ec
Omar Khaled <omarkhaled@Omars-MacBook-Pro.local>
2020-09-29 11:24:49 +0200

    fixing haversinve distance

--- src/problem.py
+++ src/problem.py
@@ -50,14 +50,14 @@ distance: the distance of the great circle arc between two points
           calculated by harversine method
 """
 
-def haversine_distance(lon1, lat1, lon2, lat2):
-
+def haversine_distance(lon1, lat1, lon2, lat2)
     # convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
 
     # haversine formula 
     dlon = lon2 - lon1 
     dlat = lat2 - lat1 
+    
     a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
     c = 2 * math.asin(math.sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles

c1fa115abcc71ed16a2b281dbbb37c57991a94aa
Omar Khaled <omarkhaled@Omars-MacBook-Pro.local>
2020-09-29 11:24:46 +0200

    fixing haversinve distance

--- src/problem.py
+++ src/problem.py
@@ -58,8 +58,8 @@ def haversine_distance(lon1, lat1, lon2, lat2):
     # haversine formula 
     dlon = lon2 - lon1 
     dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
 

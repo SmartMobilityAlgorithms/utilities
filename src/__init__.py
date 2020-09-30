37f5d723816aca8985c7b7e53cdcd737295e6452
Omar Khaled <s-omarkhaled@zewailcity.edu.eg>
2020-09-30 05:25:48 +0200

    dependencies

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,9 @@ from .common import *
 from .viz import *
 from .jupyter import *
 from .problem import *
+
+# we will need to make an independent
+# namespace for poi module because it
+# has a lot of conflicting names with
+# other existing modules
+import poi
